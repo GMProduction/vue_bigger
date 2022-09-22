0c2908432459807c88aee3e5d6702f1090630fda
pradana <pradanamahendra@gmail.com>
2022-09-22 14:05:23 +0700

    filter

--- resources/js/components/admin/content/LaporanPemasukan.vue
+++ resources/js/components/admin/content/LaporanPemasukan.vue
@@ -58,12 +58,12 @@
               </v-date-picker>
             </v-menu>
 
-            <a>Akun:</a>
+            <!-- <a>Akun:</a>
             <v-radio-group row v-model="akun" mandatory>
               <v-radio label="All" value=""></v-radio>
               <v-radio label="B" value="b"></v-radio>
               <v-radio label="R" value="r"></v-radio>
-            </v-radio-group>
+            </v-radio-group> -->
           </div>
 
           <Button
